60fa82ed0988ed0b613497384db97cde709b8f36
Kin Tat Lay <hongkongbboy@gmail.com>
2020-01-30 21:28:01 -0800

    updated style components on clickable buttons

--- react-client/src/components/ClickButtons.jsx
+++ react-client/src/components/ClickButtons.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import heart from '../../../img/heart.png';
+import repost from '../../../img/repost.png';
+import share from '../../../img/share.png';
+import nextUp from '../../../img/addToList.png';
+import more from '../../../img/more.png';
 
 class ClickButtons extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-                
+        this.state = { 
+            liked: false,
+            reposted: false,
+            addNextUp: false,
+            moreBtn: false
         };
         this.handleLikeClick = this.handleLikeClick.bind(this);
         this.handleRepostClick = this.handleRepostClick.bind(this);
         this.handleShareClick = this.handleShareClick.bind(this);
         this.handleNextUpClick = this.handleNextUpClick.bind(this);
-        this.hanleMoreClick = this.handleMoreClick.bind(this);
+        this.handleMoreClick = this.handleMoreClick.bind(this);
     };
 
     handleLikeClick() {
-
+        this.setState(state => ({
+            liked: !state.liked
+        }));
+        console.log(this.state.liked);
     };
 
     handleRepostClick() {
-
+        this.setState(state => ({
+            reposted: !state.reposted
+        }));
+        console.log(this.state.reposted);
     };
 
     handleShareClick() {
@@ -26,24 +41,323 @@ class ClickButtons extends React.Component {
     };
 
     handleNextUpClick() {
-
+        this.setState(state => ({
+            addNextUp: !state.addNextUp
+        }));
     };
 
     handleMoreClick() {
-
+        this.setState(state => ({
+            moreBtn: !state.moreBtn
+        }));
+        console.log(this.state.moreBtn);
     };
 
     render() {
         return (
-            <div>
-                <button onClick={this.handleLikeClick}>Like</button>
-                <button onClick={this.handleRepostClic}>Repost</button>
-                <button onClick={this.handleShareClick}>Share</button>
-                <button onClick={this.handleNextUpClick}>Add to Next up</button>
-                <button onClick={this.handleMoreClick}>More</button>
-            </div>
+            <GroupButtons>
+                {this.state.liked ? 
+                    <LikeButtonClicked type="button" onClick={this.handleLikeClick}>
+                        <span className="icon"><ImgClicked src={heart} /></span><TextClicked className="text">Liked</TextClicked>
+                    </LikeButtonClicked>
+                    :
+                    <LikeButton type="button" onClick={this.handleLikeClick}>
+                        <span className="icon"><Img src={heart} /></span><Text className="text">Like</Text>
+                    </LikeButton>
+                }
+                {this.state.reposted ?
+                    <RepostButtonClicked type="button" onClick={this.handleRepostClick}>
+                        <span className="icon"><ImgClicked src={repost} /></span><TextClicked className="text">Reposted</TextClicked>
+                    </RepostButtonClicked>
+                    :
+                    <RepostButton type="button" onClick={this.handleRepostClick}>
+                        <span className="icon"><Img src={repost} /></span><Text className="text">Repost</Text>
+                    </RepostButton>
+                }
+                <ShareButton type="button" onClick={this.handleShareClick}>
+                    <span className="icon"><Img src={share} /></span><Text className="text">Share</Text>
+                </ShareButton>
+                {this.state.addNextUp ?
+                    <NextUpButtonClicked type="button" onClick={this.handleNextUpClick}>
+                        <span className="icon"><ImgClicked src={nextUp} /></span><TextClicked className="text">Added</TextClicked>
+                    </NextUpButtonClicked>
+                    :
+                    <NextUpButton type="button" onClick={this.handleNextUpClick}>
+                        <span className="icon"><Img src={nextUp} /></span><Text className="text">Add to Next up</Text>
+                    </NextUpButton>
+                }
+                {this.state.moreBtn ?
+                    <MoreButtonClicked type="button" onClick={this.handleMoreClick}>
+                        <span className="icon"><ImgClicked src={more} /></span><TextClicked className="text">More</TextClicked>
+                    </MoreButtonClicked>
+                    :
+                    <MoreButton type="button" onClick={this.handleMoreClick}>
+                        <span className="icon"><Img src={more} /></span><Text className="text">More</Text>
+                    </MoreButton>
+                }            
+            </GroupButtons>
         )
     }
 }
+
+// Styled Components
+const GroupButtons = styled.div`
+    float: left;
+    vertical-align: middle;
+    display: inline-block;
+`;
+
+const LikeButton = styled.button`
+    width: 66px;
+    height: 26px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #e5e5e5;
+
+    &:hover {
+        border: 1px solid #bfbfbf;
+    }
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const LikeButtonClicked = styled.button`
+    width: 76px;
+    height: 26px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #ff5500;
+
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const RepostButton = styled.button`
+    width: 86px;
+    height: 26px;
+    border: 1px solid #e5e5e5;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #e5e5e5;
+
+    &:hover {
+        border: 1px solid #bfbfbf;
+    }
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const RepostButtonClicked = styled.button`
+    width: 100px;
+    height: 26px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #ff5500;
+
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const ShareButton = styled.button`
+    width: 76px;
+    height: 26px;
+    border: 1px solid #e5e5e5;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #e5e5e5;
+
+    &:hover {
+        border: 1px solid #bfbfbf;
+    }
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const NextUpButton = styled.button`
+    width: 136px;
+    height: 26px;
+    border: 1px solid #e5e5e5;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #e5e5e5;
+
+    &:hover {
+        border: 1px solid #bfbfbf;
+    }
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const NextUpButtonClicked = styled.button`
+    width: 136px;
+    height: 26px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #ff5500;
+
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const MoreButton = styled.button`
+    width: 72px;
+    height: 26px;
+    border: 1px solid #e5e5e5;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #e5e5e5;
+
+    &:hover {
+        border: 1px solid #bfbfbf;
+    }
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const MoreButtonClicked = styled.button`
+    width: 72px;
+    height: 26px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center; 
+    border-radius: 3px;
+    cursor: pointer;
+    background-color: white;
+    margin: 0 5px 0 0;
+    border: 1px solid #ff5500;
+
+    &:focus {
+        outline: none !important;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 30px;
+        height: 26px;
+        span.icon {display:inline-block};
+        span.text {display:none};
+    };
+`;
+
+const ImgClicked = styled.img`
+    height: 21px;
+    filter: invert(39%) sepia(100%) saturate(2820%) hue-rotate(360deg) brightness(103%) contrast(110%);
+`;
+
+const Img = styled.img`
+    height: 21px;
+`;
+
+const TextClicked = styled.span`
+    margin-right: 6px;
+    color: #ff5500;
+`;
+
+const Text = styled.span`
+    margin-right: 6px;
+`;
 
 export default ClickButtons;
--- react-client/src/components/WriteComment.jsx
+++ react-client/src/components/WriteComment.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 class WriteComment extends React.Component {
     constructor(props) {
@@ -26,12 +27,40 @@ class WriteComment extends React.Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                 <label>
-                <input placeholder="Write a comment" type="text" value={this.state.value} onChange={this.handleChange} />
+                <Input placeholder="Write a comment" type="text" value={this.state.value} onChange={this.handleChange} />
                 </label>
                 </form>
             </div>
         )
     }
 }
+
+// Styled Components
+const Input = styled.input`
+    position: absolute;
+    width: 746px;
+    height: 26px;
+    padding: 0 9px;
+    cursor: pointer;
+    border: 1px solid #e5e5e5;
+    border-radius: 4px;
+    float: left;
+    top: 50%;
+    right: 7px;
+    transform: translate(0, -50%);
+    font-size: 14px;
+
+    &:focus {
+        outline: none !important;
+        border:1px solid #999;
+    }
+
+    @media only screen and (max-width: 1220px) {
+        width: 586px;
+    }
+    @media only screen and (max-width: 1065px) {
+        width: 486px;
+    }
+`;
 
 export default WriteComment;
